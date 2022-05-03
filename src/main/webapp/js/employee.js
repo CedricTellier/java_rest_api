04fd7f2d6d235d2f186c45714ec98d3b3f96191d
Cedric Tellier <cedric.tellier25@gmail.com>
2022-05-03 20:59:31 +0200

    Async/Await for employees

--- src/main/webapp/js/employee.js
+++ src/main/webapp/js/employee.js
@@ -1,88 +1,96 @@
 async function insertNewEmployee(event) {
     event.preventDefault();
+    try {
+        let employee = {
+            firstname: document.getElementById("employeeName").value,
+            lastname: document.getElementById("employeeLastname").value,
+            age: parseInt(document.getElementById("employeeAge").value),
+            company: document.getElementById("employeeCompany").value,
+        };
 
-    let employee = {
-        firstname: document.getElementById("employeeName").value,
-        lastname: document.getElementById("employeeLastname").value,
-        age: parseInt(document.getElementById("employeeAge").value),
-        company: document.getElementById("employeeCompany").value,
-    };
+        document.getElementById("insertEmployeeForm").reset();
+        closeModal("insertEmployeeModal");
 
-    document.getElementById("insertEmployeeForm").reset();
-    closeModal("insertEmployeeModal");
+        const response = await fetch("http://localhost:8080/employees", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(employee)
+        })
 
-    fetch("http://localhost:8080/employees", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(employee)
-    }).then(response => response.json()).then(function(data) {
+        if(response.status!= 200) return;
         getEmployees();
-    })
-    .catch(function(error) {
+    }
+    catch(error){
         console.log(error);
-    });
-
+    }
     return true;
 }
 
 async function deleteEmployee(){
-    closeModal("deleteEmployeeModal");
-    fetch("http://localhost:8080/employees/" + document.getElementById("deleteEmployeeBtn").dataset.delete, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then(response => response.json()).then(function(data) {
-        getEmployees();
-    })
-        .catch(function(error) {
-            console.log(error);
+    try {
+        closeModal("deleteEmployeeModal");
+        const response = await fetch("http://localhost:8080/employees/" + document.getElementById("deleteEmployeeBtn").dataset.delete, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
         });
+
+        if(response.status != 200) return;
+        getEmployees();
+    }
+    catch(error){
+        console.log(error);
+    }
 }
 
-function getAnEmployee(id){
-    fetch("http://localhost:8080/employees/" + id, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then(response => response.json()).then(function(employee) {
+async function getAnEmployee(id){
+    try {
+        const response = await fetch("http://localhost:8080/employees/" + id, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        if(response.status != 200) return;
+        const employee = await response.json();
         document.getElementById("employeeModifyName").value = employee.firstname;
         document.getElementById("employeeModifyLastname").value = employee.lastname;
         document.getElementById("employeeModifyAge").value = employee.age;
         document.getElementById("employeeModifyCompany").value = employee.company;
-    })
-        .catch(function(error) {
-            console.log(error);
-        });
+    }
+    catch (e) {
+        console.log(e);
+    }
 }
 
-function modifyEmployee(event){
-
+async function modifyEmployee(event){
     event.preventDefault();
-    let employee = {
-        firstname: document.getElementById("employeeModifyName").value,
-        lastname: document.getElementById("employeeModifyLastname").value,
-        age: parseInt(document.getElementById("employeeModifyAge").value),
-        company: document.getElementById("employeeModifyCompany").value,
-    };
-    document.getElementById("modifyEmployeeForm").reset();
-    closeModal("modifyEmployeeModal");
+    try {
+        let employee = {
+            firstname: document.getElementById("employeeModifyName").value,
+            lastname: document.getElementById("employeeModifyLastname").value,
+            age: parseInt(document.getElementById("employeeModifyAge").value),
+            company: document.getElementById("employeeModifyCompany").value,
+        };
+        document.getElementById("modifyEmployeeForm").reset();
+        closeModal("modifyEmployeeModal");
 
-    fetch("http://localhost:8080/employees/" + document.getElementById("modifyEmployeeBtn").dataset.modify, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(employee)
-    }).then(response => response.json()).then(function(employee) {
+        const response = await fetch("http://localhost:8080/employees/" + document.getElementById("modifyEmployeeBtn").dataset.modify, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(employee)
+        });
+
+        if (response.status != 200) return;
         getEmployees();
-    })
-    .catch(function(error) {
+    }
+    catch(error){
         console.log(error);
-    });
-
+    }
     return true;
 }
